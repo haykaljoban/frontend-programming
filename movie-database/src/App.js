425af10147732b14d2cf581265237b41be8a41e8
Aufa Billah <aufaroot18@gmail.com>
2022-03-08 15:27:09 +0700

    App: render Hello Component

--- movie-database/src/App.js
+++ movie-database/src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+/**
+ * Import Component Hello
+ */
+import Hello from "./components/Hello";
 
 function App() {
   return (
     <div>
       <h2>This is Create React App</h2>
+      {/*
+        Memanggil Component Hello.
+        Mengirim props name. 
+       */}
+      <Hello name="Aufa" />
     </div>
   );
 }
