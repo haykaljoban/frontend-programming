d239e85f78cd669b83862629057e643e5926887f
Aufa Billah <aufaroot18@gmail.com>
2022-04-12 14:35:11 +0700

    Create movie pages

--- movie-database/src/pages/Home.js
+++ movie-database/src/pages/Home.js
@@ -1,22 +1,15 @@
 // Import Navbar, Hero, Movies, Footer Component
-import Navbar from "../components/Navbar/Navbar";
 import Hero from "../components/Hero/Hero";
 import Movies from "../components/Movies/Movies";
-import Footer from "../components/Footer/Footer";
 import AddMovieForm from "../components/AddMovieForm/AddMovieForm";
 import { useState } from "react";
 import data from "../utils/constants/data";
 
 /**
- * Membuat Component Main.
- * Menampung Hero dan Movies
+ * Membuat Component Home.
+ * Menampilkan Halaman Home.
  */
-function Main() {
-  /**
-   * Ini hanya snippet(potongan) code.
-   * Kode yang lainnya tetap sama.
-   */
-
+function Home() {
   /**
    * Mengangkat stave movies: lifting state.
    * Dari Component Movies ke Component Home.
@@ -25,26 +18,11 @@ function Main() {
   const [movies, setMovies] = useState(data);
 
   return (
-    <main>
+    <>
       <Hero />
       {/* Mengirim props: state movies */}
       <Movies movies={movies} setMovies={setMovies} />
       <AddMovieForm movies={movies} setMovies={setMovies} />
-    </main>
-  );
-}
-
-/**
- * Membuat Component Home.
- * Menampilkan Halaman Home.
- * Menampung Navbar, Main, dan Footer Component
- */
-function Home() {
-  return (
-    <>
-      <Navbar />
-      <Main />
-      <Footer />
     </>
   );
 }
