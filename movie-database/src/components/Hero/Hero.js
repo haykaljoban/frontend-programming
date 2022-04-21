ccd3e632581a0dce4b65c3788e5af005d9c0044b
Aufa Billah <aufaroot18@gmail.com>
2022-04-21 08:29:37 +0700

    Refactor Hero to use Fetch

--- movie-database/src/components/Hero/Hero.js
+++ movie-database/src/components/Hero/Hero.js
@@ -1,25 +1,45 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import styles from "./Hero.module.css";
+import { useEffect, useState } from "react";
 
 function Hero() {
+  // Membuat State movie
+  const [movie, setMovie] = useState("");
+
+  async function fetchMovie() {
+    const url = "https://www.omdbapi.com/?apikey=fcf50ae6&i=tt2975590";
+    // Melakukan Fetch data dari API omdb.
+    const response = await fetch(url);
+    const data = await response.json();
+
+    // Update state movie dengan data movie (hasil fetch)
+    setMovie(data);
+  }
+
+  /**
+   * Menjalankan useEffect.
+   * Parameter kedua digunakan untuk custom lifecycle update.
+   * Jika state di dalam array berubah, maka jalankan useEffect lagi (lifecycle update).
+   * Jika state di dalam array kosong, maka jalankan sekali (lifecycle mount).
+   */
+  useEffect(fetchMovie, []);
+
+  // Tampilkan state movie.
+  console.log(movie);
+
   return (
     <div className={styles.container}>
       <section className={styles.hero}>
         <div className={styles.hero__left}>
-          <h2 className={styles.hero__title}>Spiderman</h2>
-          <h3 className={styles.hero__genre}>
-            Genre: Thriller, Drama, Romance
-          </h3>
-          <p className={styles.hero__description}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam
-            cum accusamus quisquam earum velit ea nobis maiores exercitationem
-            nam temporibus.
-          </p>
+          <h2 className={styles.hero__title}>{movie.Title}</h2>
+          <h3 className={styles.hero__genre}>Genre: {movie.Genre}</h3>
+          <p className={styles.hero__description}>{movie.Plot}</p>
           <button className={styles.hero__button}>Watch</button>
         </div>
         <div className="hero__right">
           <img
             className={styles.hero__image}
-            src="https://picsum.photos/536/354"
+            src={movie.Poster}
             alt="placeholder"
           />
         </div>
