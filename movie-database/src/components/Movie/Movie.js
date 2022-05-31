84bb40e57f2a21075d088e10a2a1fa8936f9edfb
Aufa Billah <aufaroot18@gmail.com>
2022-05-31 20:15:20 +0700

    feat: create popular movie page

--- movie-database/src/components/Movie/Movie.js
+++ movie-database/src/components/Movie/Movie.js
@@ -7,9 +7,14 @@ function Movie(props) {
 
   return (
     <StyledMovie>
-      <img src={movie.poster} alt={movie.title} />
+      <img
+        src={
+          movie.poster || `https://image.tmdb.org/t/p/w300/${movie.poster_path}`
+        }
+        alt={movie.title}
+      />
       <h3>{movie.title}</h3>
-      <p>{movie.year}</p>
+      <p>{movie.year || movie.release_date}</p>
     </StyledMovie>
   );
 }
