c865967da53b3ee25519e7e97a20d31977af624d
Aufa Billah <aufaroot18@gmail.com>
2022-06-01 23:10:05 +0700

    refactor: add title props

--- movie-database/src/components/Movies/Movies.js
+++ movie-database/src/components/Movies/Movies.js
@@ -4,7 +4,7 @@ import styles from "./Movies.module.css";
 
 function Movies(props) {
   // Destructing props: state movies
-  const { movies, setMovies } = props;
+  const { title, movies, setMovies } = props;
 
   function handleClick() {
     const movie = {
@@ -26,7 +26,7 @@ function Movies(props) {
     <div>
       <div className={styles.container}>
         <section className={styles.movies}>
-          <h2 className={styles.movies__title}>Latest Movies</h2>
+          <h2 className={styles.movies__title}>{title}</h2>
           <div className={styles.movie__container}>
             {movies.map((movie) => {
               return <Movie key={movie.id} movie={movie} />;
