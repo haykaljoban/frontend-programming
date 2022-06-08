144dd69a79fbeacbcd4e77116aea3fa06e3c6fd4
Aufa Billah <aufaroot18@gmail.com>
2022-06-08 18:38:02 +0700

    refactor: provide redux store

--- movie-database/src/index.js
+++ movie-database/src/index.js
@@ -4,16 +4,26 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 // Import Browser Router dari React Router
 import { BrowserRouter } from "react-router-dom";
+// Import Provider: untuk menghubungkan React dan Redux.
+import { Provider } from "react-redux";
+import store from "./store";
 
+/**
+ * Menghubungkan React dan Redux.
+ * Bungkus App dengan Provider.
+ * Agar Component dapat mengakses Store.
+ */
 ReactDOM.render(
   <React.StrictMode>
     {/*
      * Bungkus/Hubungkan Aplikasi dengan Browser Router.
      * Agar URL browser dapat dimanipulasi oleh React Router
      */}
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
