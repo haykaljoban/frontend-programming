a6e68dca26237317d3ff9ad49f1ffbe0ca64eb96
Aufa Billah <aufaroot18@gmail.com>
2022-06-08 18:40:59 +0700

    refactor: use redux state

--- movie-database/src/components/Movies/Movies.js
+++ movie-database/src/components/Movies/Movies.js
@@ -1,9 +1,15 @@
+// Import useSelector: untuk mengakses state redux
+import { useSelector } from "react-redux";
 import Movie from "../Movie/Movie";
 import styles from "./Movies.module.css";
 
-function Movies(props) {
-  // Destructing props: state movies
-  const { title, movies } = props;
+function Movies({ title }) {
+  /**
+   * Gunakan useSelector untuk mengakses state redux.
+   * Menerima parameter state (global state).
+   * Akses state movies: state global - nama reducer - nama state
+   */
+  const movies = useSelector((state) => state.movies.movies);
 
   return (
     <div>
@@ -11,9 +17,9 @@ function Movies(props) {
         <section className={styles.movies}>
           <h2 className={styles.movies__title}>{title}</h2>
           <div className={styles.movie__container}>
-            {movies.map((movie) => {
-              return <Movie key={movie.id} movie={movie} />;
-            })}
+            {movies.map((movie) => (
+              <Movie key={movie.id} movie={movie} />
+            ))}
           </div>
         </section>
       </div>
